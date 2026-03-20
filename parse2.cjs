8773cc43809ed59f31322c33a0b5b81488bbd224
LeQuangGiaBao <baolqg1309@gmail.com>
2026-03-20 12:37:37 +0700

    feat: remove temporary API schema parsing scripts

--- parse2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const t = JSON.parse(fs.readFileSync('api.json'));
-const schema = t.paths['/vietrecruit/payment/transactions']?.get?.responses?.['200']?.content?.['*/*']?.schema;
-console.log(JSON.stringify(schema));
