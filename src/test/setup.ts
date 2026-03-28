0cf597bd72165ef776763afefb0462b74b7410de
LeQuangGiaBao <baolqg1309@gmail.com>
2026-03-28 08:37:38 +0700

    chore: Remove the entire frontend application, its configurations, and update the .gitignore.

--- src/test/setup.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '@vue/test-utils'
-import { createPinia, setActivePinia } from 'pinia'
-import { beforeEach } from 'vitest'
-
-beforeEach(() => {
-  const pinia = createPinia()
-  setActivePinia(pinia)
-  config.global.plugins = [pinia]
-})
